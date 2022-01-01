577aabd957b3d6c24fbe3d1b79e5d9f239054c95
Sunjay Varma <varma.sunjay@gmail.com>
2021-12-31 18:13:01 -0800

    Simplifying HtmlElement type by removing Array

--- src/html.rs
+++ src/html.rs
@@ -1,7 +1,5 @@
 use std::borrow::Cow;
 
-use crate::Array;
-
 #[derive(Debug, Clone, PartialEq, Hash)]
 pub struct HtmlElement {
     /// The tag name of this HTML element
@@ -9,16 +7,31 @@ pub struct HtmlElement {
     /// The HTML attributes of this element
     pub attrs: Cow<'static, [HtmlAttribute]>,
     /// The child elements or text objects under this element
-    pub children: Array<'static, HtmlChild>,
+    pub children: Vec<HtmlChild>,
 }
 
 #[derive(Debug, Clone, PartialEq, Hash)]
 pub struct HtmlAttribute {
-    //TODO
+    /// The name of the attribute
+    pub name: Cow<'static, str>,
+    /// The value of the attribute
+    pub value: Cow<'static, str>,
 }
 
 #[derive(Debug, Clone, PartialEq, Hash)]
 pub enum HtmlChild {
     Element(HtmlElement),
     Text(Cow<'static, str>),
+}
+
+impl From<HtmlElement> for HtmlChild {
+    fn from(el: HtmlElement) -> Self {
+        Self::Element(el)
+    }
+}
+
+impl From<Cow<'static, str>> for HtmlChild {
+    fn from(text: Cow<'static, str>) -> Self {
+        Self::Text(text)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-mod array;
 mod html;
 mod tags;
 
-pub use array::*;
 pub use html::*;
 pub use tags::*;
